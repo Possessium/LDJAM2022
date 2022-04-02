b61487cd1e09ee7d86cba4174e09d895765c3a22
Hugo Guerquin <hugo.guerquin@outlook.fr>
2022-04-02 16:52:00 +0200

    DestinyString done

--- Assets/Scripts/Manager/DestinyString.cs
+++ Assets/Scripts/Manager/DestinyString.cs
@@ -1,11 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DestinyString : MonoBehaviour
 {
+    [SerializeField] private Mask mask;
+    [SerializeField] private Image badVisu;
 
-    
+    [SerializeField, Range(0,1)] float value = 0;
 
+    private void Update()
+    {
+        mask.rectTransform.offsetMax = new Vector2(mask.rectTransform.offsetMax.x, Mathf.MoveTowards(1080, 0, value * 1080));
+        mask.rectTransform.offsetMin = new Vector2(mask.rectTransform.offsetMin.x, -Mathf.MoveTowards(-1080, 0, value * 1080));
 
+        badVisu.rectTransform.offsetMin = - mask.rectTransform.offsetMin;
+        badVisu.rectTransform.offsetMax = - mask.rectTransform.offsetMax;
+    }
+
+    public void SetTop (RectTransform rt, float top)
+    {
+        rt.offsetMax = new Vector2(rt.offsetMax.x, -top);
+    }
+
+    public void SetBottom(RectTransform rt, float bottom)
+    {
+        rt.offsetMin = new Vector2(rt.offsetMin.x, bottom);
+    }
 }

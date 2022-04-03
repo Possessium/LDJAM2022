d839f173ae842eb083f0d4baf51a234bf830f0dd
Hugo Guerquin <hugo.guerquin@outlook.fr>
2022-04-03 11:04:44 +0200

    Blue version placed

--- Assets/Scripts/Puzzle/PuzzleMask.cs
+++ Assets/Scripts/Puzzle/PuzzleMask.cs
@@ -6,6 +6,7 @@ public class PuzzleMask : MonoBehaviour
 {
 
     private PuzzleObject currentChild;
+    private Vector3 offsetChild;
 
     [SerializeField, Range(0, 1)] float value = 0;
 
@@ -14,13 +15,14 @@ public class PuzzleMask : MonoBehaviour
     {
         transform.position = Vector3.MoveTowards(new Vector3(.29f, 10, 7), new Vector3(.29f, .48f, 7), value * 10);
         if(currentChild)
-            currentChild.transform.localPosition = new Vector3(0, -transform.position.y, 0);
+            currentChild.transform.localPosition = new Vector3(0, -transform.position.y, 0) + offsetChild;
     }
 
     public void GetAllPieces()
     {
         currentChild = FindObjectOfType<PuzzleObject>();
         currentChild.transform.parent = transform;
+        offsetChild = currentChild.transform.position;
     }
 
     public void SetStringValue(float _value)
